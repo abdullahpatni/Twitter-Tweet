f99b9ad765db14eeb2c0bde9ac1cce8caa2000c6
Abdullah Abdul Gaffar Patni <abdullah@Abdullahs-MacBook-Air.local>
2022-10-09 18:47:49 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Tweet } from 'react-twitter-widgets';
+import ShowTweets from './components/showTweets';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <UrlBasic /> */}
+      <ShowTweets />
     </div>
   );
 }
 
+export const UrlBasic = () => (
+  <Tweet tweetId={'1139878843345281025'}  />
+);
 export default App;
---
+++ src/components/showTweets.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Tweet } from "react-twitter-widgets";
+
+function ShowTweets() {
+	const [TweetIds, setTweetIds] = useState([]);
+	useEffect(() => {
+		getDataFromAPI();
+	}, []);
+
+	function getDataFromAPI() {
+		axios.get("http://www.mocky.io/v2/5d1ef97d310000552febe99d").then((res) => {
+			let data = extractTweetIds(res.data);
+			setTweetIds(data);
+			console.log(TweetIds);
+		});
+  }
+  
+	function extractTweetIds(Tweet: any) {
+		return Tweet.map((tweet: any) => tweet._id.split("/").pop());
+	}
+
+	return (
+		<div>
+			{TweetIds.map((name, i) => (
+				<Tweet tweetId={name} key={i} />
+			))}
+		</div>
+	);
+}
+
+export default ShowTweets;
